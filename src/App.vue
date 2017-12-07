99d4e2cfb1df4492daf01f0fa446086939bf52b9
Lili0820 <1179028267@qq.com>
2017-12-07 13:55:15 +0800

    vue test

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <firstcomponent></firstcomponent>
+    <ul>
+      <li><router-link to="/first">点我跳转到第一页</router-link></li>
+      <li><router-link to="/second">点我跳转到第二页</router-link></li>
+    </ul>
+    <router-view class="view"></router-view>
+  </div>
+</template>
+
+<script>
+  import firstcomponent from './component/FirstComponent.vue'
+  import secondcomponent from './component/SecondComponent.vue'
+  import VueRouter from "vue-router";
+  import VueResource from 'vue-resource'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  },
+  components:{firstcomponent,secondcomponent}
+}
+</script>
+
+<style>
+body{
+  font-family:  Helvetica, sans-serif;
+}
+</style>
